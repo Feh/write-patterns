1f63778e40071f0e33b4780f430c4c33409572ec
Julius Plenz <julius@plenz.com>
2014-04-29 23:32:42 +0200

    Add a bunch of new checks

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-write-bench: write-bench.c
-	gcc -O2 -Wall -Werror -Wstack-protector -lrt $< -o $@
+write-bench: write-bench.c Makefile
+	gcc -O2 -Wall -Werror -Wstack-protector -Wmissing-prototypes -lrt $< -o $@
 clean:
 	rm -f write-bench
--- write-bench.c
+++ write-bench.c
@@ -5,6 +5,7 @@
 #include <sys/vfs.h>
 #include <sys/mman.h>
 #include <sys/sendfile.h>
+#include <sys/resource.h>
 #include <unistd.h>
 #include <assert.h>
 #include <time.h>
@@ -18,39 +19,38 @@ struct bench_func {
 	char *desc;
 };
 
-int cmp_dbl(const void *a, const void *b)
-{
-	return ((*(double *)a) < (*(double *)b) ? -1 : 1);
-}
+int cmp_int(const void *a, const void *b);
+int is_regular_file(int fd);
+ssize_t block_size(int fd);
+ssize_t do_sendfile(int in, int out);
+ssize_t dummy_read_file(int in, int out);
+ssize_t filesize(int fd);
+ssize_t mmap_write_advices_falloc(int in, int out);
+ssize_t mmap_write_advices(int in, int out);
+ssize_t mmap_write_advices_truncate(int in, int out);
+ssize_t mmap_write(int in, int out);
+ssize_t pipe_splice_advices_falloc(int in, int out);
+ssize_t pipe_splice_advices(int in, int out);
+ssize_t pipe_splice_advices_truncate(int in, int out);
+ssize_t pipe_splice(int in, int out);
+ssize_t read_write_16bs_advices_falloc(int in, int out);
+ssize_t read_write_16bs_advices(int in, int out);
+ssize_t read_write_16bs_advices_truncate(int in, int out);
+ssize_t read_write_16xopt(int in, int out);
+ssize_t read_write_1k(int in, int out);
+ssize_t read_write_256xopt(int in, int out);
+ssize_t read_write_4xopt(int in, int out);
+ssize_t read_write_bs(int in, int out, ssize_t bs);
+ssize_t read_write_opt_fact(int in, int out, ssize_t fact);
+ssize_t read_write_opt(int in, int out);
+ssize_t sendfile_advices_falloc(int in, int out);
+ssize_t sendfile_advices(int in, int out);
+ssize_t sendfile_advices_truncate(int in, int out);
+void advice(int in, int out);
+void do_falloc(int in, int out);
+void enlarge_truncate(int in, int out);
+void time_run(struct bench_func *arg, int in, int out);
 
-void time_run(struct bench_func *arg, int in, int out)
-{
-	int i;
-	struct timespec start, end;
-	double t[3];
-
-	assert(arg != NULL);
-
-	for(i = 0; i < sizeof(t)/sizeof(t[0]); i++) {
-		lseek(in, 0, SEEK_SET);
-		lseek(out, 0, SEEK_SET);
-		ftruncate(out, 0);
-		//usleep(500000);
-
-		clock_gettime(CLOCK_MONOTONIC, &start);
-		arg->f(in, out);
-		clock_gettime(CLOCK_MONOTONIC, &end);
-
-		t[i] = 1000 * (end.tv_sec - start.tv_sec);
-		t[i] += (end.tv_nsec - start.tv_nsec) / 1000000.0;
-	}
-
-	qsort(t, sizeof(t)/sizeof(t[0]), sizeof(t[0]), cmp_dbl);
-	fprintf(stderr, "%-30s", arg->desc);
-	for(i = 0; i < sizeof(t)/sizeof(t[0]); i++)
-		fprintf(stderr, " % 12.2fms", t[i]);
-	fprintf(stderr, "\n");
-}
 
 int is_regular_file(int fd)
 {
@@ -72,6 +72,45 @@ ssize_t filesize(int fd)
 	struct stat st;
 	assert(fstat(fd, &st) != -1);
 	return (ssize_t) st.st_size;
+}
+
+int cmp_int(const void *a, const void *b)
+{
+	return ((*(int *)a) < (*(int *)b) ? -1 : 1);
+}
+
+void time_run(struct bench_func *arg, int in, int out)
+{
+	int i;
+	struct timespec start, end;
+	struct rusage before, after;
+	int t[3];
+
+	assert(arg != NULL);
+
+	for(i = 0; i < sizeof(t)/sizeof(t[0]); i++) {
+		lseek(in, 0, SEEK_SET);
+		lseek(out, 0, SEEK_SET);
+		ftruncate(out, 0);
+		usleep(500000);
+
+		getrusage(RUSAGE_SELF, &before);
+		clock_gettime(CLOCK_MONOTONIC, &start);
+		arg->f(in, out);
+		clock_gettime(CLOCK_MONOTONIC, &end);
+		getrusage(RUSAGE_SELF, &after);
+
+		assert(arg->f == dummy_read_file || filesize(in) == filesize(out));
+
+		t[i] = 1000 * (end.tv_sec - start.tv_sec);
+		t[i] += (end.tv_nsec - start.tv_nsec) / 1000000;
+	}
+
+	qsort(t, sizeof(t)/sizeof(t[0]), sizeof(t[0]), cmp_int);
+	fprintf(stderr, "%-40s", arg->desc);
+	for(i = 0; i < sizeof(t)/sizeof(t[0]); i++)
+		fprintf(stderr, " % 8dms", t[i]);
+	fprintf(stderr, "\n");
 }
 
 ssize_t dummy_read_file(int in, int out)
@@ -200,6 +239,105 @@ ssize_t do_sendfile(int in, int out)
 	return t;
 }
 
+void advice(int in, int out)
+{
+	ssize_t t = filesize(in);
+	posix_fadvise(in, 0, t, POSIX_FADV_WILLNEED);
+	posix_fadvise(in, 0, t, POSIX_FADV_SEQUENTIAL);
+}
+
+void do_falloc(int in, int out)
+{
+	ssize_t t = filesize(in);
+	posix_fallocate(out, 0, t);
+}
+
+void enlarge_truncate(int in, int out)
+{
+	ssize_t t = filesize(in);
+	ftruncate(out, t);
+}
+
+ssize_t read_write_16bs_advices(int in, int out)
+{
+	advice(in, out);
+	return read_write_16xopt(in, out);
+}
+
+ssize_t read_write_16bs_advices_falloc(int in, int out)
+{
+	advice(in, out);
+	do_falloc(in, out);
+	return read_write_16xopt(in, out);
+}
+
+ssize_t read_write_16bs_advices_truncate(int in, int out)
+{
+	advice(in, out);
+	enlarge_truncate(in, out);
+	return read_write_16xopt(in, out);
+}
+
+ssize_t mmap_write_advices(int in, int out)
+{
+	advice(in, out);
+	return mmap_write(in, out);
+}
+
+ssize_t mmap_write_advices_falloc(int in, int out)
+{
+	advice(in, out);
+	do_falloc(in, out);
+	return mmap_write(in, out);
+}
+
+ssize_t mmap_write_advices_truncate(int in, int out)
+{
+	advice(in, out);
+	enlarge_truncate(in, out);
+	return mmap_write(in, out);
+}
+
+ssize_t pipe_splice_advices(int in, int out)
+{
+	advice(in, out);
+	return pipe_splice(in, out);
+}
+
+ssize_t pipe_splice_advices_falloc(int in, int out)
+{
+	advice(in, out);
+	do_falloc(in, out);
+	return pipe_splice(in, out);
+}
+
+ssize_t pipe_splice_advices_truncate(int in, int out)
+{
+	advice(in, out);
+	enlarge_truncate(in, out);
+	return pipe_splice(in, out);
+}
+
+ssize_t sendfile_advices(int in, int out)
+{
+	advice(in, out);
+	return do_sendfile(in, out);
+}
+
+ssize_t sendfile_advices_falloc(int in, int out)
+{
+	advice(in, out);
+	do_falloc(in, out);
+	return do_sendfile(in, out);
+}
+
+ssize_t sendfile_advices_truncate(int in, int out)
+{
+	advice(in, out);
+	enlarge_truncate(in, out);
+	return do_sendfile(in, out);
+}
+
 int main(int argc, char *argv[])
 {
 	int i;
@@ -210,10 +348,22 @@ int main(int argc, char *argv[])
 		{ read_write_opt, "read+write bs" },
 		{ read_write_4xopt, "read+write 4bs" },
 		{ read_write_16xopt, "read+write 16bs" },
+		{ read_write_16bs_advices, "read+write 16bs + advices" },
+		{ read_write_16bs_advices_falloc, "read+write 16bs + advices + falloc" },
+		{ read_write_16bs_advices_truncate, "read+write 16bs + advices + trunc" },
 		{ read_write_256xopt, "read+write 256bs" },
 		{ mmap_write, "mmap+write" },
+		{ mmap_write_advices, "mmap+write + advices" },
+		{ mmap_write_advices_falloc, "mmap+write + advices + falloc" },
+		{ mmap_write_advices_truncate, "mmap+write + advices + trunc" },
 		{ pipe_splice, "pipe+splice" },
+		{ pipe_splice_advices, "pipe+splice + advices" },
+		{ pipe_splice_advices_falloc, "pipe+splice + advices + falloc" },
+		{ pipe_splice_advices_truncate, "pipe+splice + advices + trunc" },
 		{ do_sendfile, "sendfile" },
+		{ sendfile_advices, "sendfile + advices" },
+		{ sendfile_advices_falloc, "sendfile + advices + falloc" },
+		{ sendfile_advices_truncate, "sendfile + advices + trunc" },
 	};
 
 	if(!is_regular_file(in) || !is_regular_file(out)) {
