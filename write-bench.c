3f31c497c1d02fd508dae68260a8c4c651f7263a
Julius Plenz <julius@plenz.com>
2014-04-29 21:33:11 +0200

    Do read/write in different block size

--- write-bench.c
+++ write-bench.c
@@ -34,7 +34,7 @@ void time_run(struct bench_func *arg, int in, int out)
 		clock_gettime(CLOCK_MONOTONIC, &end);
 
 		delta = 1000 * (end.tv_sec - start.tv_sec);
-		delta += (end.tv_nsec - start.tv_nsec) / 100000.0;
+		delta += (end.tv_nsec - start.tv_nsec) / 1000000.0;
 		fprintf(stderr, " % 12.2fms", delta);
 	}
 
@@ -65,23 +65,20 @@ ssize_t filesize(int fd)
 
 ssize_t dummy(int in, int out)
 {
-	ssize_t i, j = 0;
-	for(i = 0; i < 1000000; i++)
-		j += i * i * i;
-	return (ssize_t) j;
+	sleep(1);
+	return (ssize_t) 0;
 }
 
-ssize_t read_write_1k(int in, int out)
+ssize_t read_write_bs(int in, int out, ssize_t bs)
 {
 	ssize_t w = 0, r = 0, t, n, m;
-	ssize_t blocksize = 1024;
 
-	char *buf = malloc(blocksize);
+	char *buf = malloc(bs);
 	assert(buf != NULL);
 
 	t = filesize(in);
 
-	while(r < t && (n = read(in, buf, blocksize))) {
+	while(r < t && (n = read(in, buf, bs))) {
 		if(n == -1) { assert(errno == EINTR); continue; }
 		r += n;
 		while(w < r && (m = write(out, buf, (r - w)))) {
@@ -92,6 +89,38 @@ ssize_t read_write_1k(int in, int out)
 	return w;
 }
 
+ssize_t read_write_1k(int in, int out)
+{
+	return read_write_bs(in, out, 1024);
+}
+
+ssize_t read_write_opt_fact(int in, int out, ssize_t fact)
+{
+	ssize_t bs = block_size(out);
+	assert(bs >= 1024);
+	return read_write_bs(in, out, fact*bs);
+}
+
+ssize_t read_write_opt(int in, int out)
+{
+	return read_write_opt_fact(in, out, 1);
+}
+
+ssize_t read_write_4xopt(int in, int out)
+{
+	return read_write_opt_fact(in, out, 4);
+}
+
+ssize_t read_write_16xopt(int in, int out)
+{
+	return read_write_opt_fact(in, out, 16);
+}
+
+ssize_t read_write_256xopt(int in, int out)
+{
+	return read_write_opt_fact(in, out, 256);
+}
+
 int main(int argc, char *argv[])
 {
 	int i;
@@ -99,6 +128,10 @@ int main(int argc, char *argv[])
 	struct bench_func fs[] = {
 		{ dummy, "dummy" },
 		{ read_write_1k, "read+write 1k" },
+		{ read_write_opt, "read+write bs" },
+		{ read_write_4xopt, "read+write 4bs" },
+		{ read_write_16xopt, "read+write 16bs" },
+		{ read_write_256xopt, "read+write 256bs" },
 	};
 
 	if(!is_regular_file(in) || !is_regular_file(out)) {
