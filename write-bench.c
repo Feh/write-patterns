1e4bcaa6caa449278378613d5b54d8447a6e8924
Julius Plenz <julius@plenz.com>
2014-04-29 21:21:27 +0200

    Do 1K reads/writes
    This is very slow of course :-)

--- write-bench.c
+++ write-bench.c
@@ -6,19 +6,13 @@
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
 
 struct bench_func {
 	ssize_t (*f)(int, int);
 	char *desc;
 };
-
-ssize_t dummy(int in, int out)
-{
-	ssize_t i, j = 0;
-	for(i = 0; i < 1000000; i++)
-		j += i * i * i;
-	return (ssize_t) j;
-}
 
 void time_run(struct bench_func *arg, int in, int out)
 {
@@ -31,6 +25,10 @@ void time_run(struct bench_func *arg, int in, int out)
 	fprintf(stderr, "%-30s", arg->desc);
 
 	for(i = 0; i < 3; i++) {
+		lseek(in, 0, SEEK_SET);
+		lseek(out, 0, SEEK_SET);
+		ftruncate(out, 0);
+
 		clock_gettime(CLOCK_MONOTONIC, &start);
 		arg->f(in, out);
 		clock_gettime(CLOCK_MONOTONIC, &end);
@@ -65,12 +63,42 @@ ssize_t filesize(int fd)
 	return (ssize_t) st.st_size;
 }
 
+ssize_t dummy(int in, int out)
+{
+	ssize_t i, j = 0;
+	for(i = 0; i < 1000000; i++)
+		j += i * i * i;
+	return (ssize_t) j;
+}
+
+ssize_t read_write_1k(int in, int out)
+{
+	ssize_t w = 0, r = 0, t, n, m;
+	ssize_t blocksize = 1024;
+
+	char *buf = malloc(blocksize);
+	assert(buf != NULL);
+
+	t = filesize(in);
+
+	while(r < t && (n = read(in, buf, blocksize))) {
+		if(n == -1) { assert(errno == EINTR); continue; }
+		r += n;
+		while(w < r && (m = write(out, buf, (r - w)))) {
+			if(m == -1) { fprintf(stderr, "%m\n");assert(errno == EINTR); continue; }
+			w += m;
+		}
+	}
+	return w;
+}
+
 int main(int argc, char *argv[])
 {
 	int i;
-	int in = STDIN_FILENO, out = STDIN_FILENO;
+	int in = STDIN_FILENO, out = STDOUT_FILENO;
 	struct bench_func fs[] = {
 		{ dummy, "dummy" },
+		{ read_write_1k, "read+write 1k" },
 	};
 
 	if(!is_regular_file(in) || !is_regular_file(out)) {
